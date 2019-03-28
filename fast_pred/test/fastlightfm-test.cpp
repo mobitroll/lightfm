608e86a8168a107e2dbfc6ebd1cb42d775f2c40f
George Agnelli <george.agnelli@gmail.com>
2019-03-28 17:15:52 +0000

    Test loading model

--- fast_pred/test/fastlightfm-test.cpp
+++ fast_pred/test/fastlightfm-test.cpp
@@ -5,3 +5,10 @@ TEST_CASE( "FastLightFM can be constructed with defaults" ) {
     FastLightFM lfm;
     REQUIRE( !lfm.is_initialized() );
 }
+
+TEST_CASE( "FastLightFM can load model" ) {
+    FastLightFM lfm;
+    lfm.load("test/data/models/simple");
+    REQUIRE( lfm.get_no_components() == 10 );
+    REQUIRE( lfm.is_initialized() );
+}
