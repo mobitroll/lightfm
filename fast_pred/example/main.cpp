b3cf8d163874bfbd817ac453322a3699a0aa419a
fredrikl <fredrikl@kahoot.com>
2019-03-31 22:30:18 +0200

    connect python to c++

--- fast_pred/example/main.cpp
+++ fast_pred/example/main.cpp
@@ -1,17 +1,15 @@
 #include <stdio.h>
-#include "fastlightfm.h"
+#include "fastlightfm_api.h"
 #include <string>
 
 
 using namespace std;
 
 void test(const string dir) {
-    auto fastlgm = new FastLightFM();
-    fastlgm->load(dir);
-    fastlgm->dump();
-    delete fastlgm;
+    FastLFm fm = fast_lm_create();
+    fast_lm_initialize(fm, dir.c_str());
+    fast_lm_finalize(fm);
 }
-
 
 int main(int argc, const char* argv[]) {
     printf("start:\n");
