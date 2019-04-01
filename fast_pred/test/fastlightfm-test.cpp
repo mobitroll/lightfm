ff1a6fec2414d4ba035902e22ed5af903881139d
George Agnelli <george.agnelli@gmail.com>
2019-04-01 11:04:52 +0100

    WIP predict test

--- fast_pred/test/fastlightfm-test.cpp
+++ fast_pred/test/fastlightfm-test.cpp
@@ -19,11 +19,11 @@ TEST_CASE( "FastLightFM can warm-start predict" ) {
     FastLightFM lfm;
     lfm.load("test/data/models/simple");
 
-    long top_k = 3;
-    int number_of_users = 1;
-    int number_of_items = 7;
-    int user_ids_to_predict[] = { 0, 0, 0, 0, 0, 0, 0 };
-    int item_ids_to_predict[] = { 0, 1, 2, 3, 4, 5, 6 };
+    long top_k = 5;
+    int number_of_users = 5;
+    int number_of_items = 5;
+    int user_ids_to_predict[] = { 0, 0, 0, 0, 0 };
+    int item_ids_to_predict[] = { 0, 1, 2, 3, 4 };
     double predictions[number_of_items];
     long top_k_indices[top_k];
 
@@ -31,4 +31,24 @@ TEST_CASE( "FastLightFM can warm-start predict" ) {
                 number_of_users, top_k_indices, top_k);
 
     REQUIRE( predictions[0] != 0.0 );
+
+    for (int i = 0; i < top_k; i++) {
+        std::cout << top_k_indices[i] << "\n";
+    }
+
+    for (int j = 0; j < number_of_items; j++) {
+        std::cout << predictions[j] << "\n";
+    }
+
+    /* 
+        a or b -> 0 or 1 // known
+        a or b -> 0 or 1 // known
+        e -> 4
+        c -> 2
+        d -> 3
+    */
+
+    REQUIRE( top_k_indices[2] == 4 );
+    REQUIRE( top_k_indices[3] == 2 );
+    REQUIRE( top_k_indices[4] == 3 );
 }
